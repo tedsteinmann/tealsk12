ceab83ec6d6579f8236b531e5ee7f4144a993ef1
tedst <tedst@192.168.86.58>
2019-11-18 20:40:09 -0600

    chapter 3 updates

---
+++ src/chapter_3/ch3_6_console.java
@@ -0,0 +1,16 @@
+package chapter_3;
+import java.util.*; //for Scanner
+
+public class ch3_6_console {
+
+	public static void main(String[] args) {
+		
+		Scanner console = new Scanner(System.in);
+		
+		double console_input = console.nextDouble();
+		
+		System.out.println("Printing: " + console_input);
+
+	}
+
+}

dec2ee47efd5fedffc715cfba0644421c0968d94
tedst <tedst@192.168.86.53>
2020-01-02 20:51:54 -0600

    Adding an explicit constructor.

--- src/chapter_8/Point.java
+++ src/chapter_8/Point.java
@@ -1,8 +1,22 @@
 package chapter_8;
 
 public class Point {
-	int x;
-	int y;
+	
+	private int x;
+	private int y;
+	
+	public Point(int initialX, int initialY) {
+		x = initialX;
+		y = initialY;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
 	
 	// return the distance between "this" point and (0,0)
 	public double distanceFromOrigin() {
--- src/chapter_8/PointMain.java
+++ src/chapter_8/PointMain.java
@@ -4,15 +4,11 @@ public class PointMain {
 
 	public static void main(String[] args) {
 
-		Point p1 = new Point();
-		p1.x = 7;
-		p1.y = 2;
 		
-		Point p2 = new Point();
-		p2.x = 4;
-		p2.y = 3;
+		Point p1 = new Point(7, 2);		
+		// Point p2 = new Point(4,3);
 		
-		System.out.println("p1 coordinates: " + p1.x + ","  + p1.y);
+		System.out.println("p1 coordinates: " + p1.getX() + ","  + p1.getY());
 		System.out.println("p1 distance from origin: " + p1.distanceFromOrigin());
 		
 		//modify the x and y coordinates
